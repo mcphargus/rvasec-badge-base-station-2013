8ee0d3524f93135570617afec5748289db084bde
Clint Grimsley <clint.grimsley@drivefactor.com>
2013-05-30 20:14:06 -0400

    local file logging works

--- base_station.py
+++ base_station.py
@@ -6,6 +6,7 @@ import time
 import urllib2
 import urllib
 import pprint
+import datetime
 
 class BadgeInterface:
 
@@ -23,7 +24,10 @@ class BadgeInterface:
         pingId = conn.readlines()
         conn.write("t")
         conn.close()
-        return pingId
+        if pingId == None:
+            return None
+        else:
+            return pingId
 
     def getStat(self):
         conn = self.conn
@@ -31,31 +35,61 @@ class BadgeInterface:
         conn.write('\x20')
         statline = conn.readlines()
         conn.close()
-        return statline
+        if statline == None:
+            return None
+        else:
+            return statline
 
     def getBase(self):
-        baseId = uuid.uuid5(uuid.NAMESPACE_DNS,"RVASEC{HACKRVA}")
-        return baseId        
+        baseId = uuid.uuid3(uuid.NAMESPACE_DNS,str(uuid.getnode()))
+        return baseId
 
 class BaseAppUpdater:
-    url = 'http://script.google.com/macros/s/AKfycbxATodeGFzqYNKfiXggEM1uaBVX1vd52hYcya43Y7qVGQmdTZS_/exec'
+    updateFile = open('./updateFile.csv','a')
+    url = 'http://script.google.com/macros/' + \
+        's/AKfycbxATodeGFzqYNKfiXggEM1uaBVX1vd52hYcya43Y7qVGQmdTZS_/exec'
 
     def __init__(self):
         pass
 
-    def logPing(self,baseId,pings):
+    def inetCheck(self):
+        try:
+            response = urllib2.urlopen("http://www.google.com")
+            return True
+        except urllib2.URLError as err:
+            pass
+        return false
+
+    def logPingToWeb(self,baseId,pings):
         for ping in pings:
             if ping == '\r\x00':
-                continue            
+                continue
             url = self.url
-            request_headers = urllib.urlencode({"baseId":str(baseId),"pingId":ping})
+            request_headers = urllib.urlencode({"baseId":str(baseId).strip(),
+                                                "pingId":str(ping).strip().replace("Ping:","")})
             full_url = url + '?' + request_headers
             response = urllib2.urlopen(full_url)
             print response.read()
 
-badge = BadgeInterface()
-baseId = badge.getBase()
-pings = badge.getPing()
+    def logPingToFile(self,baseId,pings):
+        for ping in pings:
+            if ping == '\r\x00':
+                continue
+            updateFile = self.updateFile
+            now = datetime.datetime.now().isoformat()
+            updateFile.write("%s,%s,%s\n" % (str(now).strip(),
+                                             str(baseId).strip(),
+                                             str(ping).strip().replace('Ping:','')))
 
-updater = BaseAppUpdater()
-updater.logPing(baseId,pings)
+
+if __name__ == '__main__':
+    badge = BadgeInterface()
+    baseId = badge.getBase()
+    pings = badge.getPing()
+
+    updater = BaseAppUpdater()
+    updater.logPingToFile(baseId,pings)
+    updater.updateFile.close()
+    if updater.inetCheck():
+        updater.logPingToWeb(baseId,pings)
+
